cd671e922393fc5154e10ac31a6feb7bc1368dea
Natalia <147244357+NataliaTokhadze@users.noreply.github.com>
2023-10-25 02:56:06 +0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+# BONUS
+
+first = int(input("Write first number: "))
+second = int(input("Write second number: "))
+action = input("Write what you want to do with this nimbers: ")
+if action == "+":
+    print(first + second)
+elif action == "-":
+    print(first - second)
+elif action == "*":
+    print(first * second)
+elif action == "/":
+    print(first / second)
+elif action == "**":
+    print(first ** second)
+elif action == "%":
+    print(first / second) * 100
+else:
+    print("Use only \"+\" , \"-\", \"*\" , \"/\" , \"**\" or \"%\" ")
+
+# anu... yvelaferi sworia, magram dzalian ioli :((
+# 10-15 wutshi rac momafiqrda is davwere, magram ar mawyobs es resultati
+# ra tqma unda sheidzleba uamravi cvladis damateba, magram mainc es is ar aris hm... >:^
+# axlaxans davgoogle da amomigdo chem nairi kodi
+# mashin davaleba gaketebulia? miuxedavad amisa, me mainc ragac ar var   T.T
+# tqven gainteresebt ratom? iyidet da waikitxet axali wigni chem biografiaze da gaiget pasuxi!!!
+# P.S. sinamdvileshi tvivtonac ar vici ra ar momwons
+# ¯\_(ツ)_/¯
